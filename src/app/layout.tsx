84d046ac7b6697f7d221c693364468c76c09bb30
AbdullahEltony <abdullahmeltony@gmail.com>
2025-02-03 11:28:03 +0200

    update initial configs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} antialiased`}>
+      <body className={`${geistSans.variable} antialiased `}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>{children}</ThemeProvider>
       </body>
     </html>
